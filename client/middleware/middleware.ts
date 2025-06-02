2f13daae6d672f4b1cbd1bed83782971c27138ec
joe95-ux <ogorktabi@gmail.com>
2025-06-02 23:41:22 +0200

    modify FE middleware

--- client/middleware/middleware.ts
+++ client/middleware/middleware.ts
@@ -2,7 +2,27 @@ import type { NextRequest } from "next/server";
 import { auth0 } from "@/lib/auth0";
 
 export async function middleware(request: NextRequest) {
-  return await auth0.middleware(request);
+   const authRes = await auth0.middleware(request);
+
+    // authentication routes — let the middleware handle it
+    if (request.nextUrl.pathname.startsWith("/auth")) {
+        return authRes;
+    }
+
+    // public routes — no need to check for session
+    if (request.nextUrl.pathname === ("/")) {
+        return authRes;
+    }
+
+    const { origin } = new URL(request.url)
+    const session = await auth0.getSession()
+
+    // user does not have a session — redirect to login
+    if (!session) {
+        return NextResponse.redirect(`${origin}/auth/login`)
+    }
+
+    return authRes
 }
 
 export const config = {
